7cccd3c6e3f073a9fe308ff0fb4c2da829447814
luziming <luzimingx@gmail.com>
2019-09-24 17:17:58 +0800

    featrue:增加cdn用文件&优化select2

--- examples/assets/js/ledap-init.js
+++ examples/assets/js/ledap-init.js
@@ -1,8 +1,8 @@
 // 本示例采用了bootstrap, 为了适应界面展示，对默认模板进行变更。如果要求不高，可以直接使用系统默认模板
 // 主题代表一个工程一整个标准的组件的主题设置，一般由UI与前端进行沟通并固化。通过主题，我们能很好地实现某个工程的组件标准化.
 var themeConfig = {
-    "form-item" : {
-        template:`<component :is="tag" :class="{'has-error':showError}">
+    "form-item": {
+        template: `<component :is="tag" :class="{'has-error':showError}">
             <slot name="label" :model="model" :attr="attr">
                 <label class="col-sm-2 control-label"> {{model.getAttributeLabel(attr)}}{{model.isRequired(attr) ? '*' : ''}}</label>
             </slot>
@@ -16,15 +16,15 @@ var themeConfig = {
             </div>
         </component>`,
     },
-    "baseinput" : {
-        template : `<input class="form-control" :name="attr" :value="model[attr]" :placeholder="model.getAttributeHint(attr)" v-on="inputListeners" />`,
+    "baseinput": {
+        template: `<input class="form-control" :name="attr" :value="model[attr]" :placeholder="model.getAttributeHint(attr)" v-on="inputListeners" />`,
     },
-    "dropdown" : {
+    "dropdown": {
         template: `<select class="form-control" v-on="inputListeners">
             <option v-for="(val,key) in model.getValidatorData(attr, 'dict', 'list', {})" :value="key" :selected="key === model[attr]">{{val}}</option>
         </select>`,
     },
-    "groupinput" : {
+    "groupinput": {
         template: `<group class="btn-group" :max="dictOption.max" :excludes="dictOption.excludes" :init-value="model[attr]" :multiple="dictOption.multiple" @change="groupChange">
             <slot name="default" v-for="(val,key) in dictOption.list" :data-key="key" :value="val" :disabled="dictOption.excludes.indexOf(key) > -1 ? true : false">
                 <tab class="btn btn-default" :disabled="dictOption.excludes.indexOf(key) > -1 ? true : false" :data-key="key" :key="key"> {{val}}</tab>
@@ -32,8 +32,7 @@ var themeConfig = {
         </group>`,
     },
     searchinput: {
-        template:
-    `<div style="position: relative;">
+        template: `<div style="position: relative;">
         <input class="form-control" :name="attr" :value="value" :placeholder="model.getAttributeHint(attr)" v-on="listeners" autocomplete="off">
         <ul v-show="showList" class="list-unstyled" style="position: absolute; width: 100%; border:1px solid rgb(221, 221, 221); background-color:rgb(245, 245, 245); z-index: 10;" :style="{opacity: isHide ? 0 : 1}">
             <li v-for="(model, index) in models" @mousedown="choose(model, index, $event)" style="padding: 6px 12px; cusor: default;">
@@ -43,13 +42,13 @@ var themeConfig = {
     </div>`,
     },
     select2: {
-        template:
-`<div style="position: relative;">
-    <div class="form-control" style="height:auto">
+        template: `<div style="position: relative;">
+    <div class="form-control" style="display: flex;height:auto">
         <span v-if="multiple" v-for="model,key in selected" :key="key">
             <a class="btn btn-xs btn-default" @click="choose(model, key, $event)">{{model[itemName]}}{{'  x'}}</a>&nbsp;
         </span>
-        <input :name="attr" ref="input" :value="value" :placeholder="model.getAttributeHint(attr)" v-on="listeners" autocomplete="off" style="border-width: 0px;outline-color:white">
+        <input :name="attr" ref="input" :value="value" :placeholder="model.getAttributeHint(attr)" v-on="listeners" autocomplete="off" style="border-width: 0px;outline-color:white;flex:1">
+        <span v-if="!multiple && value" @click="clear" style="cursor: pointer;">X</span>
     </div>
     <ul v-show="showList" class="list-unstyled" style="position: absolute; width: 100%; border:1px solid rgb(221, 221, 221); background-color:rgb(245, 245, 245); z-index: 10;" :style="{opacity: isHide ? 0 : 1}">
         <li v-for="(model, index) in models" @mousedown="choose(model, index, $event)" style="padding: 6px 12px; cusor: default;" :class='{"bg-success": selected.hasOwnProperty(model[keyName])}'>
@@ -60,30 +59,30 @@ var themeConfig = {
     },
 };
 var request = function(httpOptions, suc, fail) {
-    ledap.App.axios.request(httpOptions).then(function(res){
-        suc(res.data); 
-    }).catch(function(error){
+    ledap.App.axios.request(httpOptions).then(function(res) {
+        suc(res.data);
+    }).catch(function(error) {
         fail(error);
     });
 };
 
 ledap.App.config({
-    themeConfig : themeConfig,
-    request : request,
-    webDpConfig : {
+    themeConfig: themeConfig,
+    request: request,
+    webDpConfig: {
         primaryKey: "",
-        httpRequest : function(httpOptions, suc, fail) {
-            request(httpOptions, function(data){
+        httpRequest: function(httpOptions, suc, fail) {
+            request(httpOptions, function(data) {
                 //由于接口格式问题，将其整形为标准格式
                 let tempData = {
-                    items : data,
+                    items: data,
                     sort: {},
                     meta: {
                         //由于没有后端逻辑，本处采用前端逻辑来模拟后端
-                        currentPage: httpOptions.url === "/data/dp_1.json" ?  1 : 2,
+                        currentPage: httpOptions.url === "/data/dp_1.json" ? 1 : 2,
                         pageCount: 2,
                         perPage: 20,
-                        totalCount : httpOptions.url === "/data/dp_1.json" ?  40 : 50,
+                        totalCount: httpOptions.url === "/data/dp_1.json" ? 40 : 50,
                     }
                 };
 
@@ -95,8 +94,8 @@ ledap.App.config({
 
 // 有时候我们有几套相似的组件，我们从form-item继承出来，做一个form-item1组件，把label放在尾部
 ledap.App.getTheme().addComponent({
-    name : "form-item1",
-    template:`<component :is="tag" :class="{'has-error':showError}">
+    name: "form-item1",
+    template: `<component :is="tag" :class="{'has-error':showError}">
         <div class="col-sm-10">
             <slot :model="model" :attr="attr" :checkValue="checkValue" :inputListeners="inputListeners">
                 <input class="form-control" :name="attr" :value="model[attr]" :placeholder="model.getAttributeHint(attr)" v-on="inputListeners" />
