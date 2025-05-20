2b1f175a87af7930efec0bde4ce6cb41d23eea04
chenpeng <378557980@qq.com>
2025-05-20 14:19:37 +0800

    增加Switch组件

--- src/platforms/react/components/switch/Switch.tsx
+++ src/platforms/react/components/switch/Switch.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Switch as AntSwitch, SwitchProps as AntSwitchProps } from 'antd'
+
 interface SwitchProps extends AntSwitchProps {
     onSetValue:Function;
     model:any;
-    attr:string
+    attr:string;
+    validate:Array<string>;
+    dp?:any
 }
 export default function Switch(props:SwitchProps) {
-    const { value:propValue,...reset } = props
-    return <AntSwitch checked={propValue} {...reset} />
+    const { value:propValue,onSetValue,attr,model,validate,dp,...reset } = props
+    function onChange(val){
+        onSetValue?.(val)
+    }
+    return <AntSwitch checked={propValue} onChange={onChange} {...reset} />
 }
